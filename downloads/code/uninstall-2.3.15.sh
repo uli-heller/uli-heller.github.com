131bcb0df987266b47e6649a168d3ad3b4e3feb8
Uli Heller <uli.heller@daemons-point.com>
2013-02-14 07:19:38 +0100

    Site updated at 2013-02-14 06:19:38 UTC

---
+++ downloads/code/uninstall-2.3.15.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+VERSION=2.3.15
+
+gem uninstall rails -v "${VERSION}"
+gem uninstall actionmailer -v "${VERSION}"
+gem uninstall actionpack -v "${VERSION}"
+gem uninstall activerecord -v "${VERSION}"
+gem uninstall activeresource -v "${VERSION}"
+gem uninstall activesupport -v "${VERSION}"
+#gem uninstall rack -v 1.1.1
